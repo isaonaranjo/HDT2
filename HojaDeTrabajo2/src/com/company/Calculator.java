03b7c84de6fb58c89ad2b3c0603f130debee2d91
isaonaranjo <35385804+isaonaranjo@users.noreply.github.com>
2019-01-31 21:40:20 -0600

    Add files via upload

---
+++ HojaDeTrabajo2/src/com/company/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * Hoja de trabajo 2
+ * Estructura de datos
+ * Clase de Calculadora
+ * @autor Maria Isabel Ortiz Naranjo 18176
+ * @autor Diego Estrada
+ *
+ * */
+package com.company;
+
+public interface Calculator {
+
+// Interfaz de la calculadora
+    /*@param numero1 es el primer numero a operar
+    * @param numero2 es el segundo numero a operar
+    * @param respuesta es la respuesta de la operacion indicada
+    * */
+    public int Calcular(int numero1, int numero2, String operacion);
+}
+
