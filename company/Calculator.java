a0e089734dbfd2adc28f68e5a3c392bcf19ab5bc
isaonaranjo <35385804+isaonaranjo@users.noreply.github.com>
2019-01-31 22:14:33 -0600

    Add files via upload

---
+++ company/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Hoja de trabajo 2
+ * Estructura de datos
+ * Clase de Calculadora
+ * @author Maria Isabel Ortiz Naranjo 18176
+ * @author Diego Estrada
+ *
+ *
+ * */
+package com.company;
+
+public interface Calculator {
+
+// Interfaz de la calculadora
+    /*@param numero1 es el primer numero a operar
+    * @param numero2 es el segundo numero a operar
+    * @param respuesta es la respuesta de la operacion indicada
+    * */
+    public int Calcular(int numero1, int numero2, String operacion);
+}
+
---
+++ company/Pila.java
@@ -0,0 +1,63 @@
+
+/*
+ * Hoja de trabajo 2
+ * Estructura de datos
+ * Clase de Calculadora
+ * @author Maria Isabel Ortiz Naranjo 18176
+ * @author Diego Estrada
+ * Clase: Pila
+ *
+ * */
+package com.company;
+import java.util.*;
+
+
+public class Pila <E> implements Stack<E> {
+
+    protected Vector<E> cant;
+
+    // Constructor
+    public Pila()
+    {
+        cant = new Vector<E>();
+    }
+
+    /*
+     * @param item este elemento se agregar en la parte superior de la pila.
+     * */
+    @Override
+    public void push(E item) {
+        cant.add(item);
+    }
+
+    /*
+     * @return retorna el elemento en la ultima posicion de la pila.
+     * */
+    @Override
+    public E pop() {
+        return cant.remove(size() - 1);
+    }
+
+    /*
+     * @return retorna el valor superior de la pila.
+     * */
+
+    @Override
+    public E peek() {
+        return cant.get(size() - 1);
+    }
+
+    /* @return retorna true si la pila esta vacia, y false si no lo esta.
+     * */
+    @Override
+    public boolean empty() {
+        return size() == 0;
+    }
+
+    /* @return retorna la cantidad de elementos en la pila.
+     * */
+    @Override
+    public int size() {
+        return cant.size();
+    }
+}
